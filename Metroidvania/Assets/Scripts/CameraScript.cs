a99105ed5d0d12254deba01cdbfe5099f895cf8f
FaerieFang <arschlag@gmail.com>
2018-07-11 20:34:04 -0600

    added smooth camera follow

---
+++ Metroidvania/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public GameObject player;
+    public float speed;
+
+    void Start () {
+
+    }
+	
+	void FixedUpdate () {
+
+        speed = transform.position.x - player.transform.position.x;
+        if (speed < 0){
+            speed = speed * -1;
+        }
+        float step = Time.deltaTime * speed;
+        Vector2 offset = player.transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, offset, step);
+
+    }
+}
